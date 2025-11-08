6942641e38c3969d21fc2fc3423810c5f6c1ca77
pablo-MA <molinapablo95@gmail.com>
2025-11-08 17:15:04 -0600

    Implement Rick and Morty GraphQL API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,63 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import { type TypedDocumentNode } from "@apollo/client";
+
+type GetCharactersQuery = {
+  characters: {
+    info: {
+      count: number;
+      pages: number;
+      next: number | null;
+      prev: number | null;
+    }
+    results: Array<{
+      id: string;
+      name: string;
+      status: string;
+      species: string;
+      image: string;
+    }>;
+  };
+};
+
+type GetCharactersQueryVariables = {
+  name: string
+};
+
+const GET_CHARACTERS: TypedDocumentNode<
+  GetCharactersQuery,
+  GetCharactersQueryVariables
+> = gql`
+  query GetCharacters($name: String!) {
+      characters(page: 1, filter: { name: $name }) {
+        info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        status
+        species
+        image
+      }
+    }
+  }
+`;
+
+
 function App() {
+
+  const { loading, error, data } = useQuery(GET_CHARACTERS, {
+    variables: { name: 'Morty' },
+  });
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error : {error.message}</div>;
+
+  console.log(data)
 
   return (
     <div className='p-2 max-w-7xl mx-auto'>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 import './index.css'
 import App from './App.tsx'
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://rickandmortyapi.com/graphql" }),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
