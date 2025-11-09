7fa6ff86ea60a93839aa8f415add27c426abcbca
pablo-MA <molinapablo95@gmail.com>
2025-11-09 01:16:48 -0600

    Cleanup code, remove unused imports and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,7 @@
-import { CharacterGrid } from "./components/characterGrid";
-import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
-import { type TypedDocumentNode } from "@apollo/client";
 import { Header } from "./components/header"
-import { useState, useEffect } from 'react';
-import { SearchBar } from "./components/searchBar";
-import { Error } from "@/components/error";
-import { EmptyMsg } from "@/components/emptyMsg";
-import { useDebounce } from "@/hooks/useDebounce";
-import { GridPagination } from "./components/gridPagination";
-import type { Info, Character } from "@/types/character";
+import { useState } from 'react';
 import { CharacterContainer } from "@/components/characterContainer";
 import { FavoritesContainer } from "./components/favoritesContainer";
-
-type GetCharactersQuery = {
-  characters: {
-    info: Info;
-    results: Array<Character>;
-  };
-};
-
-type GetCharactersQueryVariables = {
-  name: string;
-  page: number;
-};
-
-const GET_CHARACTERS: TypedDocumentNode<
-  GetCharactersQuery,
-  GetCharactersQueryVariables
-> = gql`
-  query GetCharacters($page: Int, $name: String!) {
-      characters(page: $page, filter: { name: $name }) {
-        info {
-        count
-        pages
-        next
-        prev
-      }
-      results {
-        id
-        name
-        status
-        species
-        image
-      }
-    }
-  }
-`;
-
 
 function App() {
   // container states
