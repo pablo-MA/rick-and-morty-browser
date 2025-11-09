024754a8cb52ce8e07c5b82992e1a468c1026d1d
pablo-MA <molinapablo95@gmail.com>
2025-11-09 01:39:38 -0600

    Update grid columns breakpoint from xl to lg

--- src/components/characterGrid.tsx
+++ src/components/characterGrid.tsx
@@ -9,7 +9,7 @@ type CharacterGridProps = {
 
 export function CharacterGrid({characters, loading = false}: CharacterGridProps) {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 my-10 gap-4 justify-items-center xl:justify-items-normal">
+        <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 my-10 gap-4 justify-items-center xl:justify-items-normal">
             {!loading 
             ? characters.map((character) => (<CharacterCard character={character}/>))
             : <CharacterGridSkeleton />}
--- src/components/favoritesGrid.tsx
+++ src/components/favoritesGrid.tsx
@@ -5,7 +5,7 @@ export function FavoritesGrid() {
   const { characters: favorites } = useFavoritesStore();
 
   return  (
-    <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 my-10 gap-4 justify-items-center xl:justify-items-normal">
+    <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 my-10 gap-4 justify-items-center xl:justify-items-normal">
         {favorites?.map((character) =><CharacterCard character={character} />)}
     </div>
   );
